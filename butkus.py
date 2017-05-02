f08e6fe3ec47e84ed15ba9e5f12963e0d81ceff7
zombiefied <zachariazbjorkman@gmail.com>
2017-05-02 19:34:20 +0200

    Add files via upload

---
+++ butkus.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import requests
+butkus=requests.get("http://butkus.xyz/api/quote")
+print (butkus.text)
+print("   ______/ \-.   _")
+print(".-/     (    o\_//")
+print(" |  ___  \_/\---'")
+print(" |_||  |_||")
